6e7bbfe777cc2a1846633dbd005255b09648e361
parth2024 <156824891+parth2024@users.noreply.github.com>
2024-08-17 12:43:51 +0530

    fixed code

--- app/routes/dashboard.events._index/route.tsx
+++ app/routes/dashboard.events._index/route.tsx
@@ -101,71 +101,71 @@ export default function EventIndex() {
         },
       }}
     >
-      {events?.length === 0 ? (
-        <main>
-          <Card className="p-4">
-            <BlockStack gap={"400"}>
-              <BlockStack gap={"300"}>
-                <Text variant="headingMd" as="h2">
-                  Define Events
-                </Text>
-              </BlockStack>
-              <div className="text-start mb-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
-                iusto consequuntur velit deserunt eius ipsam officia, quod,
-                adipisci, hic facere atque! Autem sunt sit debitis accusantium
-                sapiente, veritatis quae quibusdam?
-              </div>
-              <div className="w-full flex items-end justify-end">
-                <Button variant="primary" onClick={() => setIsModalOpen(true)}>
-                  select
-                </Button>
-              </div>
-            </BlockStack>
-          </Card>
-        </main>
-      ) : (
-        <Layout>
-          <Layout.Section>
-            <Card>
-              <DataTable
-                columnContentTypes={["text", "text", "text", "text", "text"]}
-                headings={[
-                  "Name",
-                  "Tier",
-                  "Type",
-                  "Points",
-                  "Expires In",
-                  "Actions",
-                ]}
-                rows={rows}
-                showTotalsInFooter={true}
-                sortable={[false, false, true, false]}
-              />
-            </Card>
-            <Modal
-              open={active}
-              onClose={handleClose}
-              title="Delete event"
-              primaryAction={{
-                content: "Delete",
-                onAction: handleConfirmDelete,
-                destructive: true,
-              }}
-              secondaryActions={[
-                {
-                  content: "Cancel",
-                  onAction: handleClose,
-                },
+      {/* {events?.length === 0 ? ( */}
+      {/* // <main>
+        //   <Card className="p-4">
+        //     <BlockStack gap={"400"}>
+        //       <BlockStack gap={"300"}>
+        //         <Text variant="headingMd" as="h2">
+        //           Define Events
+        //         </Text>
+        //       </BlockStack>
+        //       <div className="text-start mb-2">
+        //         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+        //         iusto consequuntur velit deserunt eius ipsam officia, quod,
+        //         adipisci, hic facere atque! Autem sunt sit debitis accusantium
+        //         sapiente, veritatis quae quibusdam?
+        //       </div>
+        //       <div className="w-full flex items-end justify-end">
+        //         <Button variant="primary" onClick={() => setIsModalOpen(true)}>
+        //           select
+        //         </Button>
+        //       </div>
+        //     </BlockStack>
+        //   </Card>
+        // </main>
+      // ) : ( */}
+      <Layout>
+        <Layout.Section>
+          <Card>
+            <DataTable
+              columnContentTypes={["text", "text", "text", "text", "text"]}
+              headings={[
+                "Name",
+                "Tier",
+                "Type",
+                "Points",
+                "Expires In",
+                "Actions",
               ]}
-            >
-              <Modal.Section>
-                <p>Do you want to delete this event?</p>
-              </Modal.Section>
-            </Modal>
-          </Layout.Section>
-        </Layout>
-      )}
+              rows={rows}
+              showTotalsInFooter={true}
+              sortable={[false, false, true, false]}
+            />
+          </Card>
+          <Modal
+            open={active}
+            onClose={handleClose}
+            title="Delete event"
+            primaryAction={{
+              content: "Delete",
+              onAction: handleConfirmDelete,
+              destructive: true,
+            }}
+            secondaryActions={[
+              {
+                content: "Cancel",
+                onAction: handleClose,
+              },
+            ]}
+          >
+            <Modal.Section>
+              <p>Do you want to delete this event?</p>
+            </Modal.Section>
+          </Modal>
+        </Layout.Section>
+      </Layout>
+      {/* )} */}
       <SelectEventModal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
